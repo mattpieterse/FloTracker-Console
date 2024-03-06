74184329228079d306749466de94161707b5807c
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-06 20:29:15 +0200

    Link local and remote repository

---
+++ FloTracker-Console/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloTracker_Console {
+    internal class App {
+        static void Main(string[] args) {
+        }
+    }
+}
