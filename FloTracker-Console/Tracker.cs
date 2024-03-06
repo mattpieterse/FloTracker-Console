d4bd70307836d516f5f936b16ade92fc3343ce21
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-06 23:13:11 +0200

    Add class documentation

--- FloTracker-Console/Tracker.cs
+++ FloTracker-Console/Tracker.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FloTracker_Console {
+
+    /// <summary>
+    /// Class to handle cycle class objects and save systems.
+    /// </summary>
     internal class Tracker {
+
     }
 }
