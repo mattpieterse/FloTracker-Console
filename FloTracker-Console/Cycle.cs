08239d275dabcccb08ad41b60d8079d4d12c4c44
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-06 22:55:46 +0200

    Add cycle class variables

--- FloTracker-Console/Cycle.cs
+++ FloTracker-Console/Cycle.cs
@@ -5,6 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FloTracker_Console {
+    
+    /// <summary>
+    /// Class to store cycle/period attribute data.
+    /// </summary>
     internal class Cycle {
+        
+        // public int UniqueID { get; set; }
+
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        // public FlowLevel flow { get; set; }
     }
+
+    internal enum FlowLevel {
+        Light,
+        Medium,
+        Heavy,
+    };
 }
