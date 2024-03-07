9498e0d378b0e353ea022cc4f3f7366148251424
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-07 22:03:24 +0200

    Update console menu to handle user input

--- FloTracker-Console/App.cs
+++ FloTracker-Console/App.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,13 +22,33 @@ namespace FloTracker_Console {
         // --
 
         static void DisplayConsoleMenu() {
+
             Sunrise.WriteLine($"{"Utilities":Cyan}");
             Sunrise.WriteLine($"{"Enter the index number of your desired option \n":DarkGray}");
             Sunrise.WriteLine($"{"(1)":Blue} Add latest cycle details");
             Sunrise.WriteLine($"{"(2)":Blue} Calculate next cycle");
             Sunrise.WriteLine($"{"(3)":Blue} Display history of tracked cycles");
             Sunrise.WriteLine($"{"(4)":Blue} Exit");
-            Sunrise.Write($"{"\nfl> ":Green}");
+            Console.WriteLine();
+
+            int choice = -1;
+            while (choice < 0 || choice > 4) {
+                try {
+                    Sunrise.Write($"{"fl> ":Green}");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
+                } catch (Exception) { 
+                    // TODO: Handle caught Exception
+                }
+            }
         }
 
         static void DisplayStartupLogo() {
