510d863e29cd706275cf6d458eca9f310dc7c9ee
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-07 13:48:16 +0200

    Add constructor link to storage class

--- FloTracker-Console/Tracker.cs
+++ FloTracker-Console/Tracker.cs
@@ -10,6 +10,12 @@ namespace FloTracker_Console {
 
     internal class Tracker {
 
-        public List<Cycle> cycles = new List<Cycle>();
+        private readonly IDataStore _dataStore;
+        public List<Cycle> cycles;
+
+        public Tracker(IDataStore dataStore) {
+            _dataStore = dataStore;
+            cycles = _dataStore.LoadData<List<Cycle>>("History.txt");
+        }
     }
 }
