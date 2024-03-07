c3b49b3fbacf34449699d8b64f91b8a1c1164fcb
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-07 14:38:19 +0200

    Add method to predict next cycle with buffer

--- FloTracker-Console/Tracker.cs
+++ FloTracker-Console/Tracker.cs
@@ -21,6 +21,24 @@ namespace FloTracker_Console {
             SaveData();
         }
 
+        public Cycle CalculateNext() {
+
+            if (cycles.Count < 2) return null;
+
+            int daysTotal = 0;
+            for (int i = 0; i < cycles.Count; i++) {
+                daysTotal += (cycles[i].startDate - cycles[i - 1].startDate).Days;
+            }
+            double avgLength = ((double) daysTotal / cycles.Count - 1);
+
+            DateTime predStart = cycles.Last().endDate.AddDays(avgLength);
+
+            return new Cycle {
+                startDate = predStart.AddDays(-2),
+                endDate = predStart.AddDays(avgLength + 2),
+            };
+        }
+
         // -- Save & Load
 
         private void SaveData() {
