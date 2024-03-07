3b37b620c9e360c9c1bce21f3e6efec71d4b14b3
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-07 13:51:36 +0200

    Add dependency injection to tracker constructor

--- FloTracker-Console/App.cs
+++ FloTracker-Console/App.cs
@@ -5,7 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FloTracker_Console {
+
     internal class App {
+
+        static IDataStore dataStore = new FileDataStore();
+        static Tracker tracker = new Tracker(dataStore);
+
         static void Main(string[] args) {
         }
     }
