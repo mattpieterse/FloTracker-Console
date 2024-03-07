35da35a7d60979946cb2a531e2f390cdeadd204e
Matt Pieterse <63480247+mpiet-za@users.noreply.github.com>
2024-03-07 19:41:02 +0200

    Update class to use sunrise utility

--- FloTracker-Console/App.cs
+++ FloTracker-Console/App.cs
@@ -21,39 +21,11 @@ namespace FloTracker_Console {
         // --
 
         static void DisplayConsoleMenu() {
-            ToConsole("\nUtilities", 1);
-            ToConsole("Enter the index number of your desired option\n");
-            ToConsole("(1) ", 0, false);
-            ToConsole("Track and add latest cycles");
-            ToConsole("(2) Calculate next cycle");
-            ToConsole("(3) Display full cycle history");
-            ToConsole("(4) Exit");
         }
 
         static void DisplayStartupLogo() {
-            ToConsole("Flo Cycle Tracker (0.1.0)", 0);
-            ToConsole("---", 0);
-        }
-
-        // --
-
-        static void ToConsole(string message, int color = 4, bool escape = true) {
-            
-            Console.ForegroundColor = color switch {
-                0 => ConsoleColor.Blue,
-                1 => ConsoleColor.Cyan,
-                2 => ConsoleColor.Magenta,
-                3 => ConsoleColor.Gray,
-                _ => ConsoleColor.White
-            };
-
-            if (escape) {
-                Console.WriteLine(message);
-            } else {
-                Console.Write(message);
-            }
-
-            Console.ResetColor();
+            Sunrise.WriteLine($"{"Flo Cycle Tracker (0.1.0)":blue}");
+            Sunrise.WriteLine($"{"---\n":blue}");
         }
     }
 }
